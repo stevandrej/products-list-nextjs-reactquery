c07a1f48a347c8c25ea8f857fd29ee024a51d348
Andrej Stevkovski <stevandrej@yahoo.com>
2023-05-27 00:39:50 +0200

    react-query config

---
+++ src/app/utils/provider.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+function Provider({ children }: React.PropsWithChildren) {
+	const [client] = useState(
+		new QueryClient({
+			defaultOptions: {
+				queries: { staleTime: 5000 },
+			},
+		})
+	);
+
+	return (
+		<QueryClientProvider client={client}>
+			{children}
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
+	);
+}
+
+export default Provider;
